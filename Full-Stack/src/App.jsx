05644401c126de78675f0a21374bde983dace4a3
John O. <jmo7728@nyu.edu>
2025-08-23 23:59:24 -0400

    Added base page and basic layout design

--- Full-Stack/src/App.jsx
+++ Full-Stack/src/App.jsx
@@ -22,41 +22,29 @@ function App() {
   return (
     <>
     <Router>
-      <nav>
-        <Link to="/">Home</Link> |{' '}
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-      <h1>Budgetr</h1>
-      <p>Full-Stack Application with Vite and React</p>
-      <p>This is a full-stack application built with Vite and React. It includes a simple API endpoint that returns a message.</p>
-      <p>Click the button below to increment the count and see the API response.</p>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+    <div className="topBar"> </div>
+    <figure className="bckGif">
+      <div className="title">
+        <h1><b>Budgetr</b>, the best way to learn finance</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <hr />
+    <div className="start">
+      
+    <Link to="/home">
+      <button className="start-btn">Start Your Journey</button>
+    </Link>
+    <Routes>
+      <Route path="/home" element={<Home />} />
+    </Routes>
+
+    </div>
+    </figure>
       <h2>API Response:</h2>
       <pre>{apiMessage}</pre>
+    
+    </Router>
     </>
+    
   );
 }
 
